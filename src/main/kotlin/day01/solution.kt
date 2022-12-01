dde3c73b3282bd19e08938a7dd3d5aa8e92158c1
Rory Collins <rory.collins@hotmail.co.uk>
2022-12-01 10:05:21 +0000

    More descriptive

--- src/main/kotlin/day01/solution.kt
+++ src/main/kotlin/day01/solution.kt
@@ -2,16 +2,17 @@ package day01
 
 import java.io.File
 
-val topThreeCalorieTotals = File("src/main/kotlin/day01/input.txt")
-    .readText()
-    .split("\n\n", "\r\n\r\n")
-    .map{elf ->
-        elf.split("\n", "\r\n").sumOf { it.toInt() }
-    }
-    .sortedDescending()
-    .take(3)
+val sortedCalorieTotals =
+    File("src/main/kotlin/day01/input.txt")
+        .readText().split("\n\n", "\r\n\r\n")
+        .map(::countCalories)
+        .sortedDescending()
 
-fun main(){
-    println("Part One: ${topThreeCalorieTotals.first()}")
-    println("Part Two: ${topThreeCalorieTotals.sum()}")
+fun countCalories(elf: String): Int {
+    return elf.split("\n", "\r\n").sumOf { it.toInt() }
+}
+
+fun main() {
+    println("Part One: ${sortedCalorieTotals.first()}")
+    println("Part Two: ${sortedCalorieTotals.take(3).sum()}")
 }
