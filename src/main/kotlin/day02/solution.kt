11d76c1ba5464058570777d5e671d4cdb89a37a2
Rory Collins <rory.collins@hotmail.co.uk>
2022-12-02 09:46:55 +0000

    Refactor for readability

--- src/main/kotlin/day02/solution.kt
+++ src/main/kotlin/day02/solution.kt
@@ -5,6 +5,7 @@ import java.io.File
 val games = File("src/main/kotlin/day02/input.txt")
     .readLines()
     .map { it.split(" ") }
+    .map { it.first() to it.last() }
 
 fun chooseShape(shape: Shape, requiredResult: String): Shape {
     return when (requiredResult) {
@@ -15,10 +16,14 @@ fun chooseShape(shape: Shape, requiredResult: String): Shape {
 }
 
 fun main() {
-    val partOneInput = games.map { Game(Shape.get(it.first()),Shape.get(it.last())) }
+    val partOneInput = games.map { (player1, player2) ->
+        Game(Shape.get(player1), Shape.get(player2))
+    }
+
     val partTwoInput = games.map { (player1, requiredResult) ->
         val playerOneShape = Shape.get(player1)
-        Game(playerOneShape, chooseShape(playerOneShape, requiredResult)) }
+        Game(playerOneShape, chooseShape(playerOneShape, requiredResult))
+    }
 
     println("Part One: ${partOneInput.sumOf { it.scoreGame() }}")
     println("Part Two: ${partTwoInput.sumOf { it.scoreGame() }}")
