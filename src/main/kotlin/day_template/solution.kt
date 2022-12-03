20fbc57a2ce539ac3d5b15b8535e7ccb5c4f58e6
Rory Collins <rory.collins@hotmail.co.uk>
2022-12-03 06:41:39 +0000

    Update template

--- src/main/kotlin/day_template/solution.kt
+++ src/main/kotlin/day_template/solution.kt
@@ -2,10 +2,10 @@ package day_template
 
 import java.io.File
 
-fun main(){
-    val input = File("src/main/kotlin/day_template/input.txt")
-        .readLines()
+val input = File("src/main/kotlin/day_template/input_test.txt")
+    .readLines()
 
+fun main(){
     println("Part One: ")
     println("Part Two: ")
 }
