9e405ba4bcb7f5ccca5432fc3d0f4af0d30e398a
Rory Collins <rory.collins@hotmail.co.uk>
2022-12-04 10:37:01 +0000

    Keep regex solution

--- src/main/kotlin/day04/solution.kt
+++ src/main/kotlin/day04/solution.kt
@@ -4,26 +4,23 @@ import java.io.File
 
 val ranges = File("src/main/kotlin/day04/input.txt")
     .readLines()
-    .map{ pair ->
-        pair.split(",")
-            .map{ convertToRange(it) }
+    .map {
+        """(\d*)-(\d*),(\d*)-(\d*)""".toRegex().find(it)!!.groups.drop(1).map { group ->
+            group!!.value.toInt()
+        }
     }
-    .map{it.first() to it.last()}
 
-fun convertToRange(assignment: String) : Set<Int> {
-    val assignments = assignment.split("-")
-        .map{it.toInt()}
-    return (assignments[0]..assignments[1]).toSet()
-}
-fun hasFullyContainedAssignment(ranges : Pair<Set<Int>, Set<Int>>) : Boolean{
-    val (range1, range2) = ranges
-    return range1.all{range2.contains(it)} || range2.all{range1.contains(it)}
-}
-fun hasPartiallyOverlappingAssignment(ranges : Pair<Set<Int>, Set<Int>>) : Boolean{
-    return ranges.first.intersect(ranges.second).any()
+fun hasFullyContainedAssignment(values: List<Int>): Boolean {
+    val (start1, end1, start2, end2) = values
+    return (start1 <= start2 && end1 >= end2) || (start2 <= start1 && end2 >= end1)
 }
 
-fun main(){
-    println("Part One: ${ranges.count{ hasFullyContainedAssignment(it) }}")
-    println("Part Two: ${ranges.count{ hasPartiallyOverlappingAssignment(it) }}")
+fun hasPartiallyOverlappingAssignment(values: List<Int>): Boolean {
+    val (start1, end1, start2, end2) = values
+    return !((end2 < start1) || (start2 > end1))
+}
+
+fun main() {
+    println("Part One: ${ranges.count { hasFullyContainedAssignment(it) }}")
+    println("Part Two: ${ranges.count { hasPartiallyOverlappingAssignment(it) }}")
 }
