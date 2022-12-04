f765f512db308ae6c62cbe6d7ee7e869fb93de11
Rory Collins <rory.collins@hotmail.co.uk>
2022-12-04 10:21:02 +0000

    Day 4 now with RegEx^TM

---
+++ src/main/kotlin/day04_regex/solution.kt
@@ -0,0 +1,26 @@
+package day04_regex
+
+import java.io.File
+
+val ranges = File("src/main/kotlin/day04/input.txt")
+    .readLines()
+    .map {
+        """(\d*)-(\d*),(\d*)-(\d*)""".toRegex().find(it)!!.groups.drop(1).map { group ->
+            group!!.value.toInt()
+        }
+    }
+
+fun hasFullyContainedAssignment(values: List<Int>): Boolean {
+    val (start1, end1, start2, end2) = values
+    return (start1 <= start2 && end1 >= end2) || (start2 <= start1 && end2 >= end1)
+}
+
+fun hasPartiallyOverlappingAssignment(values: List<Int>): Boolean {
+    val (start1, end1, start2, end2) = values
+    return !((end2 < start1) || (start2 > end1))
+}
+
+fun main() {
+    println("Part One: ${ranges.count { hasFullyContainedAssignment(it) }}")
+    println("Part Two: ${ranges.count { hasPartiallyOverlappingAssignment(it) }}")
+}
