88e1ceb2bebd8f6eb4a767616920fec3c8c91359
Rory Collins <rory.collins@hotmail.co.uk>
2022-12-05 09:36:53 +0000

    Day 5 solution

--- src/main/kotlin/day05/solution.kt
+++ src/main/kotlin/day05/solution.kt
@@ -2,38 +2,66 @@ package day05
 
 import java.io.File
 
-fun parse(manifest: String, indices: List<Int>): List<Char> {
-    return indices.map { manifest.getOrElse(it * 4 + 1) { ' ' } }
+fun parseRow(row: String, indices: List<Int>): List<Char> {
+    return indices.map { row.getOrElse(it * 4 + 1) { ' ' } }
 }
 
 fun transpose(manifest: List<List<Char>>): List<String> {
-    val indices = manifest[0].indices
-    val map = indices
+    return manifest[0].indices
         .map { index ->
             manifest.map { it[index] }
-        }
-    return map.map { it.joinToString("").trim() }
+        }.map { it.joinToString("").trim() }
 }
 
-fun processInstruction(instruction: String, cargo: List<String>): List<String> {
-    val mutableCargo = cargo.toMutableList()
-    val (count, source, target) = """move (\d*) from (\d*) to (\d*)""".toRegex().find(instruction)!!.groups.drop(1)
-        .map { it!!.value.toInt() }
-    mutableCargo[source-1] = cargo[source-1].drop(count)
-    mutableCargo[target-1] = cargo[source-1].take(count).reversed().plus(cargo[target-1])
-    return mutableCargo
+fun processInstruction(instruction: Instruction, cargo: List<String>, crateMover9000: Boolean): List<String> {
+    val processedCargo = cargo.toMutableList()
+    val count = instruction.count
+    val source = instruction.source
+    val target = instruction.target
+
+    processedCargo[source] = cargo[source].drop(count)
+    val cratesInTransit = cargo[source].take(count)
+
+    processedCargo[target] = if (crateMover9000) cratesInTransit.plus(cargo[target])
+    else cratesInTransit.reversed().plus(cargo[target])
+
+    return processedCargo
 }
+
+private fun parseInstruction(instruction: String): Instruction {
+    val (count, _source, _target) = """move (\d*) from (\d*) to (\d*)""".toRegex().find(instruction)!!.groups.drop(1)
+        .map { it!!.value.toInt() }
+    val source = _source - 1
+    val target = _target - 1
+    return Instruction(count, source, target)
+}
+
+data class Instruction(val count: Int, val source: Int, val target: Int)
 
 fun main() {
     val (manifest, instructions) = File("src/main/kotlin/day05/input.txt")
         .readText()
         .split("\n\n", "\r\n\r\n")
         .map { it.lines() }
-    val indices = manifest.last().split("""\s+""".toRegex()).drop(1).map{it.toInt() - 1 }
-    val cargo = transpose(manifest.dropLast(1).map { parse(it, indices) })
+    val indices = manifest.last().split("""\s+""".toRegex()).drop(1).map { it.toInt() - 1 }
+    val cargo = transpose(manifest.dropLast(1).map { parseRow(it, indices) })
 
-    val processedCargo = instructions.fold(cargo) { acc, next -> processInstruction(next, acc) }
+    val processedCargo = instructions
+        .map { parseInstruction(it) }
+        .fold(cargo) { acc, next -> processInstruction(next, acc, false) }
 
-    println("Part One: ${processedCargo.map { if (it.isEmpty()) "" else it.first() }.joinToString("")}")
-    println("Part Two: ")
+    val processedCargoWithCrateMover9000 = instructions
+        .map { parseInstruction(it) }
+        .fold(cargo) { acc, next -> processInstruction(next, acc, true) }
+
+    println(
+        "Part One: ${
+            processedCargo.map { if (it.isEmpty()) "" else it.first() }.joinToString("")
+        }"
+    )
+    println(
+        "Part Two: ${
+            processedCargoWithCrateMover9000.map { if (it.isEmpty()) "" else it.first() }.joinToString("")
+        }"
+    )
 }
