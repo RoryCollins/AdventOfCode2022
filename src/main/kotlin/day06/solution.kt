114dc5c8fad341a059ef2ea7a6218b465759ca74
Rory Collins <rory.collins@hotmail.co.uk>
2022-12-06 06:14:31 +0000

    refactoring

--- src/main/kotlin/day06/solution.kt
+++ src/main/kotlin/day06/solution.kt
@@ -2,14 +2,13 @@ package day06
 
 import java.io.File
 
-val input = File("src/main/kotlin/day06/input.txt")
-    .readText()
+val signal = File("src/main/kotlin/day06/input.txt").readText()
 
 fun main(){
-    println("Part One: ${lastIndexOfNUniqueCharacters(4)}")
-    println("Part Two: ${lastIndexOfNUniqueCharacters(14)}")
+    println("Part One: ${lastIndexOfNUniqueCharacters(4, signal)}")
+    println("Part Two: ${lastIndexOfNUniqueCharacters(14, signal)}")
 }
 
-private fun lastIndexOfNUniqueCharacters(n : Int) = input.windowed(n).indexOfFirst { allDifferent(n, it) } + n
+private fun lastIndexOfNUniqueCharacters(n: Int, signal: String) = signal.windowed(n).indexOfFirst { allDifferent(it) } + n
 
-private fun allDifferent(windowSize: Int, it: String) = it.toCharArray().toSet().count() == windowSize
+private fun allDifferent(window: String) = window.toSet().count() == window.length
