5e4d4f8eedda6b03ea4be0bcddbabab33c165907
Rory Collins <rory.collins@hotmail.co.uk>
2022-12-07 07:34:44 +0000

    refactor

--- src/main/kotlin/day07/Directory.kt
+++ src/main/kotlin/day07/Directory.kt
@@ -6,7 +6,6 @@ class Directory(private val name : String, val parent : Directory?){
     private lateinit var directories : List<Directory>
     private lateinit var files : List<DeviceFile>
     fun addContent(contents : List<String>){
-        directoryRegister.add(this)
         directories = contents.map{it.split(" ")}.filter{it[0] == "dir"}.map{ Directory(it[1], this) }
         files = contents.map{it.split(" ")}.filter{it[0].toIntOrNull() != null}.map{ DeviceFile(it[1], it[0].toInt()) }
     }
@@ -18,5 +17,4 @@ class Directory(private val name : String, val parent : Directory?){
     fun getSize(): Int {
         return files.sumOf { it.size } + directories.sumOf {it.getSize()}
     }
-
 }
