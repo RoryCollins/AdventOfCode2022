a08fa76ab8e23e5ccbd3df6d062c75d56ca96feb
Rory Collins <rory.collins@hotmail.co.uk>
2022-12-08 14:19:43 +0000

    reorder grid methods

--- src/main/kotlin/day08/Grid.kt
+++ src/main/kotlin/day08/Grid.kt
@@ -1,22 +1,23 @@
 package day08
 
-class Grid(private val input: List<List<Int>>){
-    fun contains(point: Coordinate) : Boolean {
+class Grid(private val input: List<List<Int>>) {
+    fun heightAt(coordinate: Coordinate): Int {
+        return input[coordinate.y][coordinate.x]
+    }
+
+    fun getAllInDirection(direction: Coordinate, coordinate: Coordinate): List<Int> {
+        val newCoordinate = coordinate.plus(direction)
+        if (this.contains(newCoordinate)) {
+            return listOf(input[newCoordinate.y][newCoordinate.x]) + getAllInDirection(direction, newCoordinate)
+        }
+        return emptyList()
+    }
+
+    private fun contains(point: Coordinate): Boolean {
         return point.x < input.first().count()
                 && point.x >= 0
                 && point.y < input.count()
                 && point.y >= 0
     }
 
-    fun getAllInDirection(direction: Coordinate, coordinate: Coordinate) : List<Int>{
-        val newCoordinate = coordinate.plus(direction)
-        if(this.contains(newCoordinate)){
-            return listOf(input[newCoordinate.y][newCoordinate.x]) + getAllInDirection(direction, newCoordinate)
-        }
-        return emptyList()
-    }
-
-    fun heightAt(coordinate: Coordinate): Int {
-        return input[coordinate.y][coordinate.x]
-    }
 }
