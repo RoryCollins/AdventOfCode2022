65ba62141029d451a0f3027dd127265891807acf
Rory Collins <rory.collins@hotmail.co.uk>
2022-12-16 12:28:05 +0000

    Day 14 part 2

--- src/main/kotlin/day14/solution.kt
+++ src/main/kotlin/day14/solution.kt
@@ -21,8 +21,8 @@ private enum class Type {
 
 private val rockBottom = rockCoordinates.minOf { it.y }
 
-private tailrec fun findBottom(coordinate: Coordinate, state: Map<Coordinate, Type>) : Pair<Boolean, Coordinate> {
-    if(coordinate.y <= rockBottom)
+private tailrec fun dropSand(coordinate: Coordinate, state: Map<Coordinate, Type>) : Pair<Boolean, Coordinate> {
+    if(coordinate.y == rockBottom)
         return true to coordinate
     val candidates =
         listOf(Coordinate(0, -1), Coordinate(-1, -1), Coordinate(1, -1))
@@ -31,34 +31,60 @@ private tailrec fun findBottom(coordinate: Coordinate, state: Map<Coordinate, Ty
     if (candidates.isEmpty()){
         return false to coordinate
     }
-    return findBottom(candidates.first(), state)
+    return dropSand(candidates.first(), state)
+}
+
+private tailrec fun dropSand2(coordinate: Coordinate, state: Map<Coordinate, Type>) : Pair<Boolean, Coordinate> {
+    val candidates =
+        listOf(Coordinate(0, -1), Coordinate(-1, -1), Coordinate(1, -1))
+            .map { coordinate.plus(it) }
+            .filter { !state.containsKey(it) }
+    if(coordinate.y == 0 && candidates.isEmpty()){
+        return true to coordinate
+    }
+    if(coordinate.y == rockBottom-1){
+        return false to coordinate
+    }
+    if (candidates.isEmpty()){
+        return false to coordinate
+    }
+    return dropSand2(candidates.first(), state)
 }
 
 
 fun main() {
     var state = rockCoordinates.associateWith { Type.ROCK }
     var exitsToTheAbyss = false
-    var count = 0;
+    var count = 0
 
     while (!exitsToTheAbyss) {
-        val foo = findBottom(Coordinate(500, 0), state)
-        exitsToTheAbyss = foo.first
-        state = state + (foo.second to Type.SAND)
+        val result = dropSand(Coordinate(500, 0), state)
+        exitsToTheAbyss = result.first
+        state = state + (result.second to Type.SAND)
         count++
     }
-    printGrid(state)
-
     println("Part One: ${count - 1}")
-    println("Part Two: ")
+
+    state = rockCoordinates.associateWith { Type.ROCK }
+    var reachedTheTop = false
+    count = 0
+
+    while (!reachedTheTop) {
+        val result = dropSand2(Coordinate(500, 0), state)
+        reachedTheTop = result.first
+        state = state + (result.second to Type.SAND)
+        count++
+    }
+    println("Part Two: $count")
 }
 
-private fun printGrid(sand: Map<Coordinate, Type>) {
-    val topLeft = Coordinate(rockCoordinates.minOf { it.x }, 9)
-    val bottomRight = Coordinate(rockCoordinates.maxOf { it.x } , rockBottom)
+private fun printGrid(map: Map<Coordinate, Type>) {
+    val topLeft = Coordinate(map.keys.minOf { it.x }, 0)
+    val bottomRight = Coordinate(map.keys.maxOf { it.x } , rockBottom-2)
 
     for (yPos in topLeft.y.downTo(bottomRight.y)) {
         for (xPos in topLeft.x..bottomRight.x) {
-            when(sand.getOrDefault(Coordinate(xPos, yPos), Type.SPACE)){
+            when(map.getOrDefault(Coordinate(xPos, yPos), Type.SPACE)){
                 Type.ROCK -> print("#")
                 Type.SAND -> print("O")
                 Type.SPACE -> print(" ")
