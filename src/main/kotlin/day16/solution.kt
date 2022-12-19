afa0dc5ee8351e14a4eda041d026ab1ac2d9335f
Rory Collins <rory.collins@hotmail.co.uk>
2022-12-19 09:13:05 +0000

    Day 16 part 1

--- src/main/kotlin/day16/solution.kt
+++ src/main/kotlin/day16/solution.kt
@@ -1,10 +1,9 @@
 package day16
 
-import shared.Coordinate
 import java.io.File
-import kotlin.time.measureTimedValue
+import java.lang.Integer.max
 
-private val valves = File("src/main/kotlin/day16/input_test.txt")
+private val valves = File("src/main/kotlin/day16/input.txt")
     .readLines()
     .map {
         """Valve (.{2}) has flow rate=(\d+); tunnel(s?) lead(s?) to valve(s?) (.*)""".toRegex()
@@ -14,47 +13,41 @@ private val valves = File("src/main/kotlin/day16/input_test.txt")
     }
 
 private val valveRegister = valves.associateBy { it.name }
-private val shortestPaths =
-    floydWarshall(valves.associate { it.name to it.connections.associateWith { 1 }.toMutableMap() }.toMutableMap())
+private val shortestPaths = floydWarshall(valves.associate { it.name to it.connections.associateWith { 1 }.toMutableMap() }.toMutableMap())
+private var score = 0
 
-fun doIt(currentValve: Valve, timeRemaining: Int, totalPressure: Int, visited: Set<Valve>) : Int{
-    if(!currentValve.open && currentValve.flowRate > 0){
-        currentValve.open = true
-        return doIt(currentValve, timeRemaining-1, totalPressure+(currentValve.flowRate*(timeRemaining-1)), visited + currentValve)
-    }
-
-    val availableValves = shortestPaths[currentValve.name]!!.keys.map{ valveRegister[it] }.filter { !visited.contains(it) }
-
-    if(availableValves.isEmpty() || timeRemaining <= 0){
-        return totalPressure
-    }
-    return availableValves
-        .maxOf{valve ->
-            val newTimeRemaining = timeRemaining - shortestPaths[currentValve.name]?.get(valve?.name)!!
-            doIt(valve!!, newTimeRemaining, totalPressure, visited+currentValve)
+private fun depthFirstSearch(currScore: Int, currentValve: String, visited: Set<String>, timeRemaining: Int) {
+    score = max(score, currScore)
+    for ((valve, dist) in shortestPaths[currentValve]!!) {
+        if (!visited.contains(valve) && timeRemaining - (dist + 1) > 0) {
+            depthFirstSearch(
+                currScore + (timeRemaining - (dist + 1)) * valveRegister[valve]!!.flowRate,
+                valve,
+                visited + valve,
+                timeRemaining - (dist + 1)
+            )
         }
+    }
 }
 
 fun main() {
-    shortestPaths.map {
-        println(it)
-    }
-    println("Part One: ${doIt(valveRegister["AA"]!!, 30, 0, emptySet())}")
-    println("Part Two: ")
+    depthFirstSearch(score, "AA", emptySet(), 30)
+
+    println("Part One: $score")
+    println("Part Two: $score")
 }
 
 
-class Valve(val name: String, val flowRate: Int, val connections: List<String>, var open: Boolean) {
-}
+data class Valve(val name: String, val flowRate: Int, val connections: List<String>, var open: Boolean)
 
 private fun floydWarshall(shortestPaths: MutableMap<String, MutableMap<String, Int>>): MutableMap<String, MutableMap<String, Int>> {
     for (k in shortestPaths.keys) {
         for (i in shortestPaths.keys) {
             for (j in shortestPaths.keys) {
-                //use `Int.MAX_VALUE/2` to avoid disaster when doing `ik + kj`
-                val ik = shortestPaths[i]?.get(k) ?: (Int.MAX_VALUE / 2)
-                val kj = shortestPaths[k]?.get(j) ?: (Int.MAX_VALUE / 2)
-                val ij = shortestPaths[i]?.get(j) ?: (Int.MAX_VALUE / 2)
+                //`Int.MAX_VALUE` blows up when doing `ik + kj`
+                val ik = shortestPaths[i]?.get(k) ?: 999
+                val kj = shortestPaths[k]?.get(j) ?: 999
+                val ij = shortestPaths[i]?.get(j) ?: 999
                 if (ik + kj < ij) {
                     shortestPaths[i]?.set(j, ik + kj)
                 }
